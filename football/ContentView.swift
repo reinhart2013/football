48240e22c84e4bb727806854b1221ea8695c9686
reinhart2013 <mduduzi.bhebhe1@icloud.com>
2025-05-30 12:27:27 +0100

    Initial Commit

---
+++ football/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  football
+//
+//  Created by Mduduzi Bhebhe on 29/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    VStack {
+        Text("What is Football?")
+            .font(.largeTitle)
+            .fontWeight(.thin)
+            .foregroundStyle(.green)
+        
+        HStack {
+            Image(systemName: "figure.australian.football")
+                .resizable()
+                .foregroundStyle(.blue)
+                .scaledToFit()
+            Image(systemName: "figure.soccer")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.red)
+            
+            Image(systemName: "figure.american.football")
+                .resizable()
+                .scaledToFit()
+            .foregroundStyle(.purple)
+            
+            
+            }
+            
+            
+                    }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Mduduzi Bhebhe on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
